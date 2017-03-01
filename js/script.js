9c040012ce420ca29c2c352e4767d4a174bd397f
jarek borzecki <jarek borzecki>
2017-03-01 12:43:36 +0100

    Add pictures to the first slider

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 
-$(document).ready(function() {
-	// Hiding and showing navigation-arrow
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > 600) {
-			$('.navigation-arrow').fadeIn();
+// $(document).ready(function() {
+// 	// Hiding and showing navigation-arrow
+// // 	$(window).scroll(function() {
+// // 		if ($(window).scrollTop() > 600) {
+// // 			$('.navigation-arrow').css('opacity', '0');
 
-		} else {
-			$('.navigation-arrow').fadeOut();
-		}
-	});
-})
+// // 		} else {
+// // 			$('.navigation-arrow').fadeOut();
+// // 		}
+// // 	});
+// })
+
+
+$(document).scroll(function() {    
+var scroll = $(this).scrollTop();
+if (scroll >= 500) {
+$(".icon").addClass("arrow-opacity");
+}
+});
